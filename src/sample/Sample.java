4bfc92edc3f94405e4f7c680fe7b9a1c2d798242
agnesshai <agnesshailendra@gmail.com>
2022-03-08 16:03:20 +0530

    br4

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -4,6 +4,11 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Sample");
+		System.out.println("Sample");
+		System.out.println("Sample");
+		System.out.println("Sample");
+		System.out.println("Sample");	System.out.println("Sample");
+		System.out.println("Sample");
 	}
 
 }
